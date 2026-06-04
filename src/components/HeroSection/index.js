b2f54232567e2f38ca03c3f539e03617a8da4e05
sonu kumar <sonukumarroy1000@gmail.com>
2026-06-04 05:38:18 +0530

    changes done for new photo

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import {Bio} from '../../data/constants';
 import Typewriter from "typewriter-effect";
-import HeroImg from "../../images/HeroImages.jpg";
+import HeroImg from "../../images/HeroImages.jpeg";
 import HeroBgAnimation from '../HeroBgAnimation';
 
 const HeroContainer = styled.div`
